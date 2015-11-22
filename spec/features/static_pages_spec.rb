cb1aeca14ab9b222b8bc28db6bf756258556ee17
Ben <ben.c.hawker@gmail.com>
2015-11-22 11:20:21 +0800

    Adds title and specs

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "StaticPages" do
     it "displays Sample app" do
       visit static_pages_home_path
       expect(page).to have_content('Sample App')
+      expect(page).to have_selector('h1', :text => 'Sample App')
+    end
+
+    it "has the correct title" do
+      visit static_pages_home_path
+      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial | Home")
     end
   end
 
@@ -13,13 +19,25 @@ describe "StaticPages" do
   	it "displays Help" do
   		visit static_pages_help_path
   		expect(page).to have_content('Help')
+      expect(page).to have_selector('h1', :text => 'Help')
   	end
+
+    it "has the correct title" do
+      visit static_pages_help_path
+      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial | Home")
+    end
   end
 
   describe "About Page" do
     it "displays About" do
       visit static_pages_about_path
       expect(page).to have_content('About')
+      expect(page).to have_selector('h1', :text => 'About')
+    end
+
+    it "has the correct title" do
+      visit static_pages_about_path
+      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial | Home")
     end
   end
 end
