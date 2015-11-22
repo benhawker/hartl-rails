10eb76acd16bab940aa44ffe3dec8cbd453eb2b6
Ben <ben.c.hawker@gmail.com>
2015-11-22 11:51:53 +0800

    Test refactor

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,39 +5,41 @@ describe "StaticPages" do
   describe "Home page" do
     it "displays Sample app" do
       visit static_pages_home_path
-      expect(page).to have_content('Sample App')
-      expect(page).to have_selector('h1', :text => 'Sample App')
+      expect(page).to have_selector('h1', text: 'Sample App')
     end
 
-    it "has the correct title" do
+    it "has the base title" do
       visit static_pages_home_path
-      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial")
+    end
+
+    it "should not have a custom page title" do
+      visit static_pages_home_path
+      expect(page).to_not have_title('| Home')
     end
   end
 
   describe "Help Page" do
   	it "displays Help" do
   		visit static_pages_help_path
-  		expect(page).to have_content('Help')
-      expect(page).to have_selector('h1', :text => 'Help')
+      expect(page).to have_selector('h1', text: 'Help')
   	end
 
     it "has the correct title" do
       visit static_pages_help_path
-      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial | Help")
     end
   end
 
   describe "About Page" do
     it "displays About" do
       visit static_pages_about_path
-      expect(page).to have_content('About')
-      expect(page).to have_selector('h1', :text => 'About')
+      expect(page).to have_selector('h1', text: 'About')
     end
 
     it "has the correct title" do
       visit static_pages_about_path
-      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial | About")
     end
   end
 end
